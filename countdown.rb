a0b34de7bed92aaaa90adc8be397ae06f6cc263c
Peter Kim <peterckim96@gmail.com>
2018-05-31 19:37:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,12 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
   
+  return "HAPPY NEW YEAR!"
+end
 
 countdown(10)
